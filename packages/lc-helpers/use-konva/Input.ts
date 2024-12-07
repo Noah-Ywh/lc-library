7ff1cb6ebb0eec81a1630957661c714bcec8efc1
Noah <noah4882@163.com>
2024-12-07 05:10:23 +0000

    fix(Konva Input): 更好的输入效果

--- packages/lc-helpers/use-konva/Input.ts
+++ packages/lc-helpers/use-konva/Input.ts
@@ -5,27 +5,34 @@ export type InputConfig = Konva.TextConfig & {
   backgroundColor?: string
   /** 输入框边框颜色 */
   borderColor?: string
+  /** 输入事件 */
+  input?: (text: string) => void
   /** 失焦事件 */
   blur?: (text: string) => void
 }
 
 export class Input extends Konva.Text {
-  #container: HTMLDivElement
+  #edit: HTMLDivElement
+
+  #editPadding = 5
 
   #backgroundColor: string
   #borderColor: string
+  #input: ((text: string) => void) | undefined
   #blur: ((text: string) => void) | undefined
 
   constructor(config: InputConfig) {
     super(config)
 
-    this.#container = document.createElement('div')
+    this.#edit = document.createElement('div')
 
     this.#backgroundColor = config.backgroundColor || 'white'
     this.#borderColor = config.borderColor || 'black'
+    this.#input = config.input
     this.#blur = config.blur
 
     this.on('click', this.#handleClick)
+    this.on('changeScale', this.#changeScale)
 
     this.destroy = this.destroy.bind(this)
   }
@@ -35,60 +42,100 @@ export class Input extends Konva.Text {
     if (!stage) {
       return
     }
-
     const container = stage.container()
+    const scale = stage.scale()
     const { x, y } = this.absolutePosition()
+    const left = x - this.#editPadding * (scale?.x || 1)
+    const top = y - this.#editPadding * (scale?.y || 1)
 
-    this.#container.className = 'lc-helpers-konva-edit'
-    this.#container.setAttribute('contenteditable', 'true')
-    this.#container.innerText = this.text()
+    this.#edit.className = 'lc-helpers-konva-edit'
+    this.#edit.setAttribute('contenteditable', 'true')
+    this.#edit.innerText = this.text()
 
-    Object.assign(this.#container.style, {
+    Object.assign(this.#edit.style, {
       fontFamily: this.fontFamily(),
       fontSize: `${this.fontSize()}px`,
       lineHeight: this.lineHeight(),
       color: this.fill(),
-      minWidth: `${this.width() + 10}px`,
-      padding: '5px',
+      minWidth: `${this.width()}px`,
+      padding: `${this.#editPadding}px`,
       backgroundColor: this.#backgroundColor,
-      border: `1px solid ${this.#borderColor}`,
+      outline: `1px solid ${this.#borderColor}`,
       borderRadius: '2px',
       position: 'absolute',
-      left: `${x - 6}px`,
-      top: `${y - 6}px`,
+      left: `${left}px`,
+      top: `${top}px`,
       zIndex: '2',
+      transform: `scale(${scale?.x || 1}, ${scale?.y || 1})`,
+      transformOrigin: 'top left',
     })
 
-    this.#container.addEventListener('blur', this.#updateText)
-    this.#container.addEventListener('keydown', this.#preventEnter)
+    this.#edit.addEventListener('input', this.#eventInput)
+    this.#edit.addEventListener('blur', this.#updateText)
+    this.#edit.addEventListener('keydown', this.#eventKeydown)
 
-    container.appendChild(this.#container)
-    this.#container.focus()
+    container.appendChild(this.#edit)
+    this.#edit.focus()
 
     const range = document.createRange()
     const selection = window.getSelection()
-    range.selectNodeContents(this.#container)
+    range.selectNodeContents(this.#edit)
     range.collapse(false)
     selection?.removeAllRanges()
     selection?.addRange(range)
   }
 
+  #eventInput = () => {
+    const text = this.#edit.innerText.trim()
+    this.#input?.(text)
+  }
+
   #updateText = () => {
-    const text = this.#container.innerText
+    const text = this.#edit.innerText.trim()
     this.text(text)
     this.#blur?.(text)
 
-    this.#container?.remove()
+    // 移除事件监听器
+    this.#edit.removeEventListener('input', this.#updateText)
+    this.#edit.removeEventListener('blur', this.#updateText)
+    this.#edit.removeEventListener('keydown', this.#eventKeydown)
+    this.#edit.remove()
   }
 
-  #preventEnter = (event: KeyboardEvent) => {
+  #eventKeydown = (event: KeyboardEvent) => {
     if (event.key === 'Enter') {
       event.preventDefault()
     }
+    if (event.key === 'Backspace' || event.key === 'Delete') {
+      event.stopPropagation()
+    }
+  }
+
+  #changeScale = () => {
+    const stage = this.getStage()
+    if (!stage) {
+      return
+    }
+    const { x, y } = this.absolutePosition()
+    const scale = stage.scale()
+
+    const left = x - this.#editPadding * (scale?.x || 1)
+    const top = y - this.#editPadding * (scale?.y || 1)
+
+    Object.assign(this.#edit.style, {
+      left: `${left}px`,
+      top: `${top}px`,
+      transform: `scale(${scale?.x || 1}, ${scale?.y || 1})`,
+    })
   }
 
   public destroy() {
     this.off('click', this.#handleClick)
+    this.off('changeScale', this.#changeScale)
+    this.#edit?.removeEventListener('input', this.#updateText)
+    this.#edit?.removeEventListener('blur', this.#updateText)
+    this.#edit?.removeEventListener('keydown', this.#eventKeydown)
+    this.#edit?.remove()
 
     super.destroy()
 
