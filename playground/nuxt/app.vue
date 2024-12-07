531633fa5c12590031f955e7289ee6e149ffeb2a
Noah <noah4882@163.com>
2024-12-07 05:11:10 +0000

    chore: 更新预览效果

--- playground/nuxt/app.vue
+++ playground/nuxt/app.vue
@@ -1,11 +1,17 @@
 <template>
-  <NuxtLayout>
+  <div class="lc-library-playground">
     <NuxtPage />
-  </NuxtLayout>
+  </div>
 </template>
 <style lang="scss">
 body {
   padding: 0;
   margin: 0;
+}
+.lc-library-playground {
+  box-sizing: border-box;
+  min-width: 100vw;
+  min-height: 100vh;
+  padding: 20px;
 }
 </style>
--- playground/nuxt/pages/helpers.client.vue
+++ playground/nuxt/pages/helpers.client.vue
@@ -6,39 +6,44 @@ import TextEditVue from '../components/TextEdit.vue'
 
 const stageRef = ref<HTMLDivElement | null>(null)
 /** 舞台 */
-const stage = ref<Konva.Stage>()
+const stage = new Konva.Stage({ container: document.createElement('div'), draggable: true })
 /** 图层 */
-const layer = ref<Konva.Layer>()
+const layer = new Konva.Layer()
 
 const props = ref({
   text: '插槽内容',
 })
 
+const stageScale = ref(1)
+
+/** 键盘按下处理函数 */
+function handleKeydown(event: KeyboardEvent) {
+  if (event.key === 'Backspace' || event.key === 'Delete') {
+    console.log('删除')
+  }
+}
+
 onMounted(() => {
   if (stageRef.value) {
-    stage.value = new Konva.Stage({
-      container: stageRef.value,
-      draggable: true,
-    })
+    stageRef.value?.addEventListener('keydown', handleKeydown)
+    stageRef.value.innerHTML = ''
+    stage.container(stageRef.value)
     useDomSizeChange(stageRef.value, ({ width, height }) => {
-      if (stage.value) {
-        stage.value.width(width)
-        stage.value.height(height)
-      }
+      stage.width(width)
+      stage.height(height)
     })
 
-    layer.value = new Konva.Layer()
-
-    stage.value?.add(layer.value)
+    stage.add(layer)
 
     const input = new Input({
+      name: 'input',
       x: 0,
       y: 25,
       width: 200,
       text: '输入框',
       fill: 'red',
       fontSize: 14,
-      lineHeight: 1.4,
+      // lineHeight: 1.4,
       align: 'left',
       verticalAlign: 'middle',
       fontFamily: 'Calibri',
@@ -46,9 +51,15 @@ onMounted(() => {
       wrap: 'none',
       backgroundColor: 'yellow',
       borderColor: 'black',
+      input(text) {
+        // const { width } = input.measureSize(text)
+        // input.width(width)
+
+        props.value.text = text
+      },
       blur(text) {
-        const { width } = input.measureSize(text)
-        input.width(width)
+        // const { width } = input.measureSize(text)
+        // input.width(width)
 
         props.value.text = text
       },
@@ -63,13 +74,70 @@ onMounted(() => {
         draggable: true,
         fill: 'blue',
       },
-      stage.value,
+      stage,
     )
 
     slot.mount(TextEditVue, props.value)
 
-    layer.value.add(slot, input)
-    layer.value?.draw()
+    layer.add(slot, input)
+
+    layer.draw()
+
+    // 鼠标滚动事件
+    stage.on('wheel', ({ evt }) => {
+      if (evt.ctrlKey || evt.metaKey) {
+        /** 画布缩放 */
+        evt.preventDefault()
+        const scaleBy = 1.1
+        const oldScale = stage.scaleX()
+        const pointer = stage.getPointerPosition()
+
+        if (pointer) {
+          const mousePointTo = {
+            x: (pointer.x - stage.x()) / oldScale,
+            y: (pointer.y - stage.y()) / oldScale,
+          }
+
+          stageScale.value = evt.deltaY > 0 ? oldScale / scaleBy : oldScale * scaleBy
+          stageScale.value = Math.max(0.4, Math.min(stageScale.value, 4))
+
+          stage.scale({ x: stageScale.value, y: stageScale.value })
+
+          const newPos = {
+            x: pointer.x - mousePointTo.x * stageScale.value,
+            y: pointer.y - mousePointTo.y * stageScale.value,
+          }
+
+          stage.position(newPos)
+          stage.fire('dragmove')
+
+          // const input = layer.findOne('.input')
+          input?.fire('changeScale')
+
+          stage.batchDraw()
+        }
+      } else {
+        // 画布滚动
+
+        /** 滚动系数 */
+        const scrollFactor = 1.1
+        /** 滚动前舞台坐标 */
+        const oldPos = stage.position()
+        /** 滚动后舞台坐标 */
+        const newPos = oldPos
+
+        if (evt.altKey) {
+          newPos.x = oldPos.x - evt.deltaY * scrollFactor
+        } else {
+          newPos.x = oldPos.x - evt.deltaX * scrollFactor
+          newPos.y = oldPos.y - evt.deltaY * scrollFactor
+        }
+
+        stage.position(newPos)
+
+        stage.batchDraw()
+      }
+    })
   }
 })
 </script>
@@ -83,7 +151,8 @@ onMounted(() => {
 <style lang="scss" scoped>
 .lc-helpers {
   width: 100%;
-  height: 300px;
+  height: 500px;
+  border: 1px solid #000;
   .canvas-container {
     width: 100%;
     height: 100%;
