33b06351f67e975141189240b48faadb2aae7bb7
Noah <noah4882@163.com>
2024-12-10 22:22:22 +0000

    fix(Konva Input): 优化使用体验

--- packages/lc-helpers/use-konva/Input.ts
+++ packages/lc-helpers/use-konva/Input.ts
@@ -1,10 +1,15 @@
 import Konva from 'konva'
 
 export type InputConfig = Konva.TextConfig & {
+  styleType?: 'outlined'
   /** 输入框背景色 */
   backgroundColor?: string
   /** 输入框边框颜色 */
   borderColor?: string
+  /** 占位符 */
+  placeholder?: string
+  /** 焦点事件 */
+  focus?: (text: string) => void
   /** 输入事件 */
   input?: (text: string) => void
   /** 失焦事件 */
@@ -12,22 +17,27 @@ export type InputConfig = Konva.TextConfig & {
 }
 
 export class Input extends Konva.Text {
-  #edit: HTMLDivElement
+  editEl: HTMLDivElement
 
-  #editPadding = 5
-
+  #config: InputConfig
+  #editPadding = 0
   #backgroundColor: string
   #borderColor: string
+  #focus: ((text: string) => void) | undefined
   #input: ((text: string) => void) | undefined
   #blur: ((text: string) => void) | undefined
 
   constructor(config: InputConfig) {
     super(config)
 
-    this.#edit = document.createElement('div')
+    this.editEl = document.createElement('div')
 
+    this.#config = config
+
+    this.#editPadding = config.padding || 0
     this.#backgroundColor = config.backgroundColor || 'white'
     this.#borderColor = config.borderColor || 'black'
+    this.#focus = config.focus
     this.#input = config.input
     this.#blur = config.blur
 
@@ -45,23 +55,31 @@ export class Input extends Konva.Text {
     const container = stage.container()
     const scale = stage.scale()
     const { x, y } = this.absolutePosition()
-    const left = x - this.#editPadding * (scale?.x || 1)
-    const top = y - this.#editPadding * (scale?.y || 1)
+    const left = Math.ceil(x - (scale?.x || 1))
+    const top = Math.ceil(y - (scale?.y || 1))
 
-    this.#edit.className = 'lc-helpers-konva-edit'
-    this.#edit.setAttribute('contenteditable', 'true')
-    this.#edit.innerText = this.text()
+    this.editEl.className = 'lc-helpers-konva-edit'
+    this.editEl.setAttribute('contenteditable', 'true')
+    this.editEl.innerText = this.text()
 
-    Object.assign(this.#edit.style, {
+    Object.assign(this.editEl.style, {
+      boxSizing: 'border-box',
+      overfiow: 'hidden',
       fontFamily: this.fontFamily(),
       fontSize: `${this.fontSize()}px`,
       lineHeight: this.lineHeight(),
       color: this.fill(),
-      minWidth: `${this.width()}px`,
+      width: `${Math.ceil(this.width() + this.#editPadding)}px`,
       padding: `${this.#editPadding}px`,
       backgroundColor: this.#backgroundColor,
-      outline: `1px solid ${this.#borderColor}`,
-      borderRadius: '2px',
+      outline: 'none',
+      borderWidth: '1px',
+      borderStyle: 'solid',
+      borderColor:
+        this.#config.styleType === 'outlined'
+          ? this.#borderColor
+          : `transparent transparent ${this.#borderColor} transparent`,
+      borderRadius: this.#config.styleType === 'outlined' ? '2px' : 0,
       position: 'absolute',
       left: `${left}px`,
       top: `${top}px`,
@@ -70,36 +88,43 @@ export class Input extends Konva.Text {
       transformOrigin: 'top left',
     })
 
-    this.#edit.addEventListener('input', this.#eventInput)
-    this.#edit.addEventListener('blur', this.#updateText)
-    this.#edit.addEventListener('keydown', this.#eventKeydown)
+    this.editEl.addEventListener('input', this.#eventInput)
+    this.editEl.addEventListener('blur', this.#updateText)
+    this.editEl.addEventListener('keydown', this.#eventKeydown)
 
-    container.appendChild(this.#edit)
-    this.#edit.focus()
+    container.appendChild(this.editEl)
+
+    this.editEl.focus()
+
+    this.#focus?.(this.text())
 
     const range = document.createRange()
     const selection = window.getSelection()
-    range.selectNodeContents(this.#edit)
+    range.selectNodeContents(this.editEl)
     range.collapse(false)
     selection?.removeAllRanges()
     selection?.addRange(range)
   }
 
   #eventInput = () => {
-    const text = this.#edit.innerText.trim()
+    // 当内容为空时，清除浏览器默认添加的 <br> 标签
+    if (this.editEl.innerHTML === '<br>') {
+      this.editEl.innerHTML = ''
+    }
+    const text = this.editEl.innerText.trim()
     this.#input?.(text)
   }
 
   #updateText = () => {
-    const text = this.#edit.innerText.trim()
+    const text = this.editEl.innerText.trim()
     this.text(text)
     this.#blur?.(text)
 
     // 移除事件监听器
-    this.#edit.removeEventListener('input', this.#updateText)
-    this.#edit.removeEventListener('blur', this.#updateText)
-    this.#edit.removeEventListener('keydown', this.#eventKeydown)
-    this.#edit.remove()
+    this.editEl.removeEventListener('input', this.#updateText)
+    this.editEl.removeEventListener('blur', this.#updateText)
+    this.editEl.removeEventListener('keydown', this.#eventKeydown)
+    this.editEl.remove()
   }
 
   #eventKeydown = (event: KeyboardEvent) => {
@@ -119,10 +144,10 @@ export class Input extends Konva.Text {
     const { x, y } = this.absolutePosition()
     const scale = stage.scale()
 
-    const left = x - this.#editPadding * (scale?.x || 1)
-    const top = y - this.#editPadding * (scale?.y || 1)
+    const left = Math.ceil(x - (scale?.x || 1))
+    const top = Math.ceil(y - (scale?.y || 1))
 
-    Object.assign(this.#edit.style, {
+    Object.assign(this.editEl.style, {
       left: `${left}px`,
       top: `${top}px`,
       transform: `scale(${scale?.x || 1}, ${scale?.y || 1})`,
@@ -132,10 +157,10 @@ export class Input extends Konva.Text {
   public destroy() {
     this.off('click', this.#handleClick)
     this.off('changeScale', this.#changeScale)
-    this.#edit?.removeEventListener('input', this.#updateText)
-    this.#edit?.removeEventListener('blur', this.#updateText)
-    this.#edit?.removeEventListener('keydown', this.#eventKeydown)
-    this.#edit?.remove()
+    this.editEl?.removeEventListener('input', this.#updateText)
+    this.editEl?.removeEventListener('blur', this.#updateText)
+    this.editEl?.removeEventListener('keydown', this.#eventKeydown)
+    this.editEl?.remove()
 
     super.destroy()
 
